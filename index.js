f64b03414a9b12a59338a8139efe2a3faa48ffd4
luc-abayo <luc.bayo@gmail.com>
2018-06-21 15:12:53 +0200

    redifined index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log(res);
+  res.send({ hi: "Hello world" });
 });
 
 const PORT = process.env.PORT || 5000;
