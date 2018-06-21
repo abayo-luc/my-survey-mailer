6c305016b60a8961d51322c262fdfd03e37638a3
luc-abayo <luc.bayo@gmail.com>
2018-06-21 23:03:24 +0200

    finished auth flaw

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+//the order here really matters, you have to initialize mongoose before using em in passport
+require("./models/User");
+require("./services/passport");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "Hello world" });
-});
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
