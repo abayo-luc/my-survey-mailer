fef3f9acca2a0d044dad713d68de8606974a622c
luc-abayo <luc.bayo@gmail.com>
2018-06-22 04:31:46 +0200

    added client side

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+import { FETCH_USER } from "../actions/types";
+const initialState = null;
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
+    default:
+      return state;
+  }
+}
